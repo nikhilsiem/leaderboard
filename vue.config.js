dc35833431aacc656bc49095834b8c3d69ad7744
Nikhil Singh <nikhils.iem@gmail.com>
2025-02-05 15:04:38 +0530

    commit for hosting

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/leaderboard/"
+  publicPath: process.env.NODE_ENV === 'production' ? '/leaderboard/' : '/'
 })
